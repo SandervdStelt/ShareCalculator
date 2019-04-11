c68da7db52468216468d0d8b39151d7fcfecdc02
SandervdStelt <sander.vanderstelt@gmail.com>
2019-04-11 10:11:58 +0200

    Update ShareCalcWIP.py

--- ShareCalcWIP.py
+++ ShareCalcWIP.py
@@ -1,44 +1,46 @@
 
 import pandas as pd
-
+import glob
 ### Import fudara timeseries
 
+# define some directories
+production_database_path = r'C:\Projects\ShareCalculator\LocatieProductie.xlsx'
+fudura_import_path = glob.glob(r'C:\Projects\ShareCalculator\Fudura Import\*.xlsx')
 # Script to read excel containing timeseries pasting into excel with timeseries
-excel_file = pd.read_excel (r'C:\Projects\ShareCalculator\Opwek_Maart.xlsx',sheet_name = 1,header = 0,parse_cols = "A,C") 
+excel_file = pd.read_excel (fudura_import_path[0],sheet_name = 1,header = 0,parse_cols = "A,C") 
 excel_file.columns = ["Date","Total"]
 
-# define empty cell
-empty = ' '
-# Find first empty row in timeseries to determine enddate
-first_empty_row = excel_file.index[excel_file.iloc[:,0] == empty][0] 
-# Get total consumption
-totaal_verbruik = excel_file.iloc[0:first_empty_row,[0,1]]
-# multiply by -1
-totaal_verbruik.iloc[:,1] *= -1
-# convert date column to datetimeformat
-totaal_verbruik["Date"] = pd.to_datetime(totaal_verbruik["Date"])
-# Set datetime as index
-totaal_verbruik.index = totaal_verbruik["Date"]
-# delete datecolumn
-del totaal_verbruik["Date"]
+# Modify data to store in database
+empty = ' ' # define empty cell
+first_empty_row = excel_file.index[excel_file.iloc[:,0] == empty][0] # Find first empty row in timeseries to determine enddate
+totaal_verbruik = excel_file.iloc[0:first_empty_row,[0,1]] # Get total consumption
+totaal_verbruik.iloc[:,1] *= -1 # multiply by -1
+totaal_verbruik["Date"] = pd.to_datetime(totaal_verbruik["Date"]) # convert date column to datetimeformat
+totaal_verbruik.index = totaal_verbruik["Date"] # Set datetime as index
+del totaal_verbruik["Date"] # delete datecolumn
 
-
-###
-test_database = pd.read_excel(r'C:\Projects\ShareCalculator\TestDatabase.xlsx',sheet_name = 0,header = 0,parse_cols = "A,B") 
+# Get database (currently an excel file)
+test_database = pd.read_excel(production_database_path,sheet_name = 0,header = 0,parse_cols = "A,B") 
 test_database.columns = ["Date","Total"]
-test_database["Date"] = pd.to_datetime(test_database["Date"])
-test_database.index = test_database["Date"]
-del test_database["Date"]
+test_database["Date"] = pd.to_datetime(test_database["Date"]) # convert date column to datetimeformat
+test_database.index = test_database["Date"] # Set datetime as index
+del test_database["Date"] # delete datecolumn
+
 
 # use concat to paste new dataset into old
 new_database = pd.concat([test_database, totaal_verbruik])
-new_database = new_database[~new_database.index.duplicated(keep = 'last')]
+new_database = test_database.merge(totaal_verbruik,how = 'left',right_index = True,left_index = True)
+#new_database = new_database[~new_database.index.duplicated(keep = 'last')]
 print(new_database)
 # Store Excel file
-
-writer = pd.ExcelWriter(r'C:\Projects\ShareCalculator\TestDatabase.xlsx', engine = 'xlsxwriter')
+"""
+writer = pd.ExcelWriter(production_database_path, engine = 'xlsxwriter')
 new_database.to_excel(writer)
 writer.save()
+"""
+
+
+#
 
 
 
